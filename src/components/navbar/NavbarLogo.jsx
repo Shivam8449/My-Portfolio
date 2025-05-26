f36cd054715fc43a481d08e43c9063c0e3723159
Shivam8449 <shivamthakur9632@gmail.com>
2025-05-27 00:24:45 +0530

    text

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -5,7 +5,7 @@ const NavbarLogo = () => {
         Shivam Thakur
       </h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-        AQ
+        ST
       </h1>
     </div>
   );
